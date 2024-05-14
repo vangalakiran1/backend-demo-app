58d35524f8394c9fbcc3290df041209d67599ca6
kiran <kk143187@gmail.com>
2024-05-14 15:08:33 +0530

    demo-app

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("get data");
+});
+
+app.get("/about", (request, response) => {
+  response.send("get about data");
+});
+
+app.get("/blogs", (request, response) => {
+  response.send("get blogs data");
+});
+
+app.listen("1234", () => {
+  console.log("server is running port 1234");
+});
