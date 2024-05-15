6396949a7625be37b6c5b42f9fd87ba3d64a3a0f
vangalakiran1 <80635284+vangalakiran1@users.noreply.github.com>
2024-05-15 18:53:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (request, response) => {
-  response.send("get data, this is main branch data");
+  response.send("get data, this is branch1 data");
 });
 
 app.get("/about", (request, response) => {
